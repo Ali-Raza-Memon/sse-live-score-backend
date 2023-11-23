0abbca71dc0517d08a785ce6ecd892e544c83206
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-11-23 21:42:16 +0500

    updated

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const wss = new WebSocket.Server({ server });
 
 // Map to store connected clients
 const clients = new Map();
-
+var count = 1;
 wss.on('connection', function connection(ws) {
     console.log('A new client Connected!');
     ws.send('Welcome to live Score App');
@@ -19,12 +19,60 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         console.log('received from', clientId, ':', message);
 
-        // Broadcast the message to all other clients
-        clients.forEach(function each(client, id) {
-            if (id !== clientId && client.readyState === WebSocket.OPEN) {
-                client.send(`${clientId}: ${message}`);
+                   
+            console.log("Hello I am from String. 2");
+            // Handle string message
+            console.log('Received string message:', message);
+
+            if(count === 0){
+            // Broadcast the string message to all other clients
+            clients.forEach(function each(client, id) {
+                if (id !== clientId && client.readyState === WebSocket.OPEN) {
+                    client.send(`${clientId}: ${message}`);
+                }
+            });
+            return;
             }
-        });
+            else{
+            // Handle JSON message
+            try {
+                count=0;
+                const parsedMessage = JSON.parse(message);
+
+                // Check if the parsed message has the required properties
+                if (parsedMessage && typeof parsedMessage.type === 'string') {
+
+                    const parsedMessage = JSON.parse(message);
+
+                    if (parsedMessage.type === 'create-teams') {
+                        const { team1, team2 } = parsedMessage;
+                        console.log('Received team creation message:', team1, team2);
+                
+                        // Broadcast the team creation message to all clients
+                        clients.forEach(function each(client) {
+                            if (client.readyState === WebSocket.OPEN) {
+                                client.send(`New Teams Created: ${team1} vs ${team2}`);
+                            }
+                        });
+                    } else {
+                        // Handle other types of JSON messages
+                        console.log('Received from', clientId, ':', message);
+
+                        // Broadcast the JSON message to all other clients
+                        clients.forEach(function each(client, id) {
+                            if (id !== clientId && client.readyState === WebSocket.OPEN) {
+                                client.send(`${clientId}: ${message}`);
+                            }
+                        });
+                    }
+                } else {
+                    console.error('Received invalid JSON message:', message);
+                }
+            } catch (error) {
+                console.error('Error parsing message as JSON:', error);
+            }
+        }
+        
     });
 
     ws.on('close', function () {
@@ -32,6 +80,8 @@ wss.on('connection', function connection(ws) {
         clients.delete(clientId);
     });
 });
+
+
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
