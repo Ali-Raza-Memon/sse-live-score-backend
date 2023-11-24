e3ee0eb74114db9684c8921bce8483be3406cef9
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-11-24 10:20:56 +0500

    working...

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const clients = new Map();
 var count = 1;
 wss.on('connection', function connection(ws) {
     console.log('A new client Connected!');
-    ws.send('Welcome to live Score App');
+    ws.send(JSON.stringify({ type: 'intial', message: 'Welcome to live Score App' }));
+
     // Generate a unique identifier for the client
     const clientId = Math.random().toString(36).substring(7);
     clients.set(clientId, ws);
@@ -22,56 +23,109 @@ wss.on('connection', function connection(ws) {
                    
             console.log("Hello I am from String. 2");
             // Handle string message
-            console.log('Received string message:', message);
+            // console.log('Received string message 111111111111111:', message);
 
-            if(count === 0){
+            const parsedMessage = JSON.parse(message);
+            console.log('Received string message 111111111111111:', parsedMessage);
+
+
+            if(parsedMessage.type==='update-score'){
+                console.log("UPPPPPPPPPPPPPPPPdate is called");
+                
             // Broadcast the string message to all other clients
-            clients.forEach(function each(client, id) {
-                if (id !== clientId && client.readyState === WebSocket.OPEN) {
-                    client.send(`${clientId}: ${message}`);
-                }
-            });
-            return;
+                clients.forEach(function each(client) {
+                    if (client.readyState === WebSocket.OPEN) {
+                        // client.send(`${clientId}: ${message}`);
+                        
+                        client.send(JSON.stringify({ type: 'update-score', team_wise_score: ` ${message} ` }));
+
+                    }
+                });
+            }
+            else if(parsedMessage.type==='create-teams'){
+
+                const { team1, team2 } = parsedMessage;
+                console.log('Received team creation message:', team1, team2);
+                    
+                // Broadcast the team creation message to all clients
+                clients.forEach(function each(client) {
+                    console.log("team-01 :"+team1 + " ......team-02 :"+team2);
+                    if (client.readyState === WebSocket.OPEN) {
+                        client.send(JSON.stringify({ type: 'create-teams', message: `${team1} vs ${team2}` }));
+                    }
+                });
+                        
             }
             else{
-            // Handle JSON message
-            try {
-                count=0;
-                const parsedMessage = JSON.parse(message);
-
-                // Check if the parsed message has the required properties
-                if (parsedMessage && typeof parsedMessage.type === 'string') {
-
-                    const parsedMessage = JSON.parse(message);
-
-                    if (parsedMessage.type === 'create-teams') {
-                        const { team1, team2 } = parsedMessage;
-                        console.log('Received team creation message:', team1, team2);
-                
-                        // Broadcast the team creation message to all clients
-                        clients.forEach(function each(client) {
-                            if (client.readyState === WebSocket.OPEN) {
-                                client.send(`New Teams Created: ${team1} vs ${team2}`);
-                            }
-                        });
-                    } else {
-                        // Handle other types of JSON messages
-                        console.log('Received from', clientId, ':', message);
-
-                        // Broadcast the JSON message to all other clients
-                        clients.forEach(function each(client, id) {
-                            if (id !== clientId && client.readyState === WebSocket.OPEN) {
-                                client.send(`${clientId}: ${message}`);
-                            }
-                        });
-                    }
-                } else {
-                    console.error('Received invalid JSON message:', message);
-                }
-            } catch (error) {
-                console.error('Error parsing message as JSON:', error);
+                console.log(parsedMessage.type);
             }
-        }
+
+            // if (parsedMessage){
+            //     console.log("I am true!!!!!!!!!!!!")
+            //     switch (parsedMessage.type){
+            //         case 'create-teams':
+            //             // Handle team creation message
+            //             const { team1, team2 } = parsedMessage;
+            //             console.log('Received team creation message:', team1, team2);
+            //             // Handle the team creation logic
+            //             break;
+
+            //         case 'update-scores':
+            //                 // Handle score update message
+            //             const { scoreData } = parsedMessage;
+            //             console.log('Received score update message:', scoreData);
+            //                 // Handle the score update logic
+            //             break;
+
+            //         default:
+            //                 console.log('Unknown message type:', parsedMessage.type);
+            //     }
+            // }else {
+            //     console.error('Received invalid JSON message:', message);
+            // }
+
+
+
+           
+    
+            // Handle JSON message
+            // try {
+            //     count=0;
+            //     const parsedMessage = JSON.parse(message);
+
+            //     // Check if the parsed message has the required properties
+            //     if (parsedMessage && typeof parsedMessage.type === 'string') {
+            //         console.log(parsedMessage.type);
+            //         const parsedMessage = JSON.parse(message);
+
+            //         if (parsedMessage.type === 'create-teams') {
+            //             const { team1, team2 } = parsedMessage;
+            //             console.log('Received team creation message:', team1, team2);
+                
+            //             // Broadcast the team creation message to all clients
+            //             clients.forEach(function each(client) {
+            //                 if (client.readyState === WebSocket.OPEN) {
+            //                     client.send(`New Teams Created: ${team1} vs ${team2}`);
+            //                 }
+            //             });
+            //         } else {
+            //             // Handle other types of JSON messages
+            //             console.log('Received from', clientId, ':', message);
+
+            //             // Broadcast the JSON message to all other clients
+            //             clients.forEach(function each(client, id) {
+            //                 if (id !== clientId && client.readyState === WebSocket.OPEN) {
+            //                     client.send(`${clientId}: ${message}`);
+            //                 }
+            //             });
+            //         }
+            //     } else {
+            //         console.error('Received invalid JSON message:', message);
+            //     }
+            // } catch (error) {
+            //     console.error('Error parsing message as JSON:', error);
+            // }
+        
         
     });
 
